1369a8303d66c5c83594305be93fbaff5187c1bd
Crasneanu Cristian <crasneanu.cristian@gmail.com>
2019-06-18 14:51:58 +0300

    [ Entity Level ] Add parameters handler and session id parameter

--- stphotomap/src/main/java/com/streetography/stphotomap/models/parameters/Parameters.kt
+++ stphotomap/src/main/java/com/streetography/stphotomap/models/parameters/Parameters.kt
@@ -3,18 +3,6 @@ package com.streetography.stphotomap.models.parameters
 public typealias KeyValue = Pair<String, String>
 
 public class Parameters {
-    companion object {
-        fun defaultParameters(): ArrayList<KeyValue> {
-            return arrayListOf(
-                KeyValue(Keys.basemap, "yes"),
-                KeyValue(Keys.shadow, "yes"),
-                KeyValue(Keys.sort, "popular"),
-                KeyValue(Keys.tileSize, "256"),
-                KeyValue(Keys.pinOptimize, "4")
-            )
-        }
-    }
-
     public class Keys {
         companion object {
             public val basemap = "basemap"
--- stphotomap/src/main/java/com/streetography/stphotomap/scenes/stphotomap/builders/STPhotoMapUriBuilder.kt
+++ stphotomap/src/main/java/com/streetography/stphotomap/scenes/stphotomap/builders/STPhotoMapUriBuilder.kt
@@ -7,23 +7,24 @@ import com.streetography.stphotomap.extensions.uri.excludeParameter
 import com.streetography.stphotomap.models.parameters.KeyValue
 import com.streetography.stphotomap.models.parameters.Parameters
 import com.streetography.stphotomap.models.tile_coordinate.TileCoordinate
+import com.streetography.stphotomap.scenes.stphotomap.parameters.STPhotoMapParametersHandler
 
 class STPhotoMapUriBuilder {
-    fun geojsonTileUri(tileCoordinate: TileCoordinate, parameters: ArrayList<KeyValue> = Parameters.defaultParameters()): Pair<String, String> {
+    fun geojsonTileUri(tileCoordinate: TileCoordinate, parameters: ArrayList<KeyValue> = STPhotoMapParametersHandler.instance.parameters): Pair<String, String> {
         return this.tileUri(BuildConfig.GEOJSON_TILE_URL, tileCoordinate, parameters)
     }
 
-    fun jpegTileUri(tileCoordinate: TileCoordinate, parameters: ArrayList<KeyValue> = Parameters.defaultParameters()): Pair<String, String> {
+    fun jpegTileUri(tileCoordinate: TileCoordinate, parameters: ArrayList<KeyValue> =  STPhotoMapParametersHandler.instance.parameters): Pair<String, String> {
         return this.tileUri(BuildConfig.JPEG_TILE_URL, tileCoordinate, parameters)
     }
 
-    private fun tileUri(template: String, tileCoordinate: TileCoordinate, parameters: ArrayList<KeyValue> = Parameters.defaultParameters()): Pair<String, String> {
+    private fun tileUri(template: String, tileCoordinate: TileCoordinate, parameters: ArrayList<KeyValue> =  STPhotoMapParametersHandler.instance.parameters): Pair<String, String> {
         val downloadUri = this.tileUri(template, tileCoordinate.zoom, tileCoordinate.x, tileCoordinate.y, parameters)
         val keyUri = downloadUri.excludeParameter(KeyValue(Parameters.Keys.bbox, ""))
         return Pair(keyUri.toString(), downloadUri.toString())
     }
 
-    fun tileUri(template: String, z: Int, x: Int, y: Int, parameters: ArrayList<KeyValue> = Parameters.defaultParameters()): Uri {
+    fun tileUri(template: String, z: Int, x: Int, y: Int, parameters: ArrayList<KeyValue> =  STPhotoMapParametersHandler.instance.parameters): Uri {
         val uriString = String.format(template, z, x, y)
         val uri = Uri.parse(uriString)
         return uri.addParameters(parameters)
